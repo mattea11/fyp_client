4d8f1fdfc3f0f6f2fc643d32d009bba78aaa450d
mattea11 <matteafenech@gmail.com>
2023-06-26 23:07:29 +0200

    cleaning

--- src/aspects/_asp_rover_mon0.aj
+++ src/aspects/_asp_rover_mon0.aj
@@ -26,8 +26,8 @@ before () : (call(* *.larva_check_valid_action2(..)) && !cflow(adviceexecution()
 synchronized(_asp_rover_mon0.lock){
 
 _cls_rover_mon0 _cls_inst = _cls_rover_mon0._get_cls_rover_mon0_inst();
-_cls_inst._call(thisJoinPoint.getSignature().toString(), 36/*invalid_command*/);
-_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 36/*invalid_command*/);
+_cls_inst._call(thisJoinPoint.getSignature().toString(), 4/*invalid_command*/);
+_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 4/*invalid_command*/);
 }
 }
 before () : (call(* *.get_change_data(..)) && !cflow(adviceexecution()) && !cflow(within(larva.*))  && !(within(larva.*))) {
@@ -35,8 +35,8 @@ before () : (call(* *.get_change_data(..)) && !cflow(adviceexecution()) && !cflo
 synchronized(_asp_rover_mon0.lock){
 
 _cls_rover_mon0 _cls_inst = _cls_rover_mon0._get_cls_rover_mon0_inst();
-_cls_inst._call(thisJoinPoint.getSignature().toString(), 32/*get_change_data*/);
-_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 32/*get_change_data*/);
+_cls_inst._call(thisJoinPoint.getSignature().toString(), 0/*get_change_data*/);
+_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 0/*get_change_data*/);
 }
 }
 before ( String message) : (call(* *.send(..)) && args(message) && !cflow(adviceexecution()) && !cflow(within(larva.*))  && !(within(larva.*))) {
@@ -45,8 +45,8 @@ synchronized(_asp_rover_mon0.lock){
 
 _cls_rover_mon0 _cls_inst = _cls_rover_mon0._get_cls_rover_mon0_inst();
 _cls_inst.message = message;
-_cls_inst._call(thisJoinPoint.getSignature().toString(), 40/*send_command*/);
-_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 40/*send_command*/);
+_cls_inst._call(thisJoinPoint.getSignature().toString(), 8/*send_command*/);
+_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 8/*send_command*/);
 }
 }
 before ( double change_speed) : (call(* *.check_speed(..)) && args(change_speed) && !cflow(adviceexecution()) && !cflow(within(larva.*))  && !(within(larva.*))) {
@@ -55,8 +55,8 @@ synchronized(_asp_rover_mon0.lock){
 
 _cls_rover_mon0 _cls_inst = _cls_rover_mon0._get_cls_rover_mon0_inst();
 _cls_inst.change_speed = change_speed;
-_cls_inst._call(thisJoinPoint.getSignature().toString(), 44/*check_speed*/);
-_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 44/*check_speed*/);
+_cls_inst._call(thisJoinPoint.getSignature().toString(), 12/*check_speed*/);
+_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 12/*check_speed*/);
 }
 }
 before () : (call(* *.larva_check_valid_action1(..)) && !cflow(adviceexecution()) && !cflow(within(larva.*))  && !(within(larva.*))) {
@@ -64,8 +64,8 @@ before () : (call(* *.larva_check_valid_action1(..)) && !cflow(adviceexecution()
 synchronized(_asp_rover_mon0.lock){
 
 _cls_rover_mon0 _cls_inst = _cls_rover_mon0._get_cls_rover_mon0_inst();
-_cls_inst._call(thisJoinPoint.getSignature().toString(), 34/*valid_command*/);
-_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 34/*valid_command*/);
+_cls_inst._call(thisJoinPoint.getSignature().toString(), 2/*valid_command*/);
+_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 2/*valid_command*/);
 }
 }
 before ( double distance,double w,double x,double y) : (call(* *.check_vert_ang(..)) && args(distance,x,y,w) && !cflow(adviceexecution()) && !cflow(within(larva.*))  && !(within(larva.*))) {
@@ -77,8 +77,8 @@ _cls_inst.distance = distance;
 _cls_inst.w = w;
 _cls_inst.x = x;
 _cls_inst.y = y;
-_cls_inst._call(thisJoinPoint.getSignature().toString(), 42/*check_nav*/);
-_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 42/*check_nav*/);
+_cls_inst._call(thisJoinPoint.getSignature().toString(), 10/*check_nav*/);
+_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 10/*check_nav*/);
 }
 }
 before ( double change_ang) : (call(* *.check_vert_ang(..)) && args(change_ang) && !cflow(adviceexecution()) && !cflow(within(larva.*))  && !(within(larva.*))) {
@@ -87,8 +87,8 @@ synchronized(_asp_rover_mon0.lock){
 
 _cls_rover_mon0 _cls_inst = _cls_rover_mon0._get_cls_rover_mon0_inst();
 _cls_inst.change_ang = change_ang;
-_cls_inst._call(thisJoinPoint.getSignature().toString(), 46/*check_vert_ang*/);
-_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 46/*check_vert_ang*/);
+_cls_inst._call(thisJoinPoint.getSignature().toString(), 14/*check_vert_ang*/);
+_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 14/*check_vert_ang*/);
 }
 }
 before ( String message) : (call(* *.broadcast(..)) && args(message) && !cflow(adviceexecution()) && !cflow(within(larva.*))  && !(within(larva.*))) {
@@ -97,8 +97,8 @@ synchronized(_asp_rover_mon0.lock){
 
 _cls_rover_mon0 _cls_inst = _cls_rover_mon0._get_cls_rover_mon0_inst();
 _cls_inst.message = message;
-_cls_inst._call(thisJoinPoint.getSignature().toString(), 38/*send_move_on*/);
-_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 38/*send_move_on*/);
+_cls_inst._call(thisJoinPoint.getSignature().toString(), 6/*send_move_on*/);
+_cls_inst._call_all_filtered(thisJoinPoint.getSignature().toString(), 6/*send_move_on*/);
 }
 }
 }
--- src/larva/_cls_rover_mon0.java
+++ src/larva/_cls_rover_mon0.java
@@ -21,7 +21,7 @@ public static LinkedHashMap<_cls_rover_mon0,_cls_rover_mon0> _cls_rover_mon0_ins
 static{
 try{
 RunningClock.start();
-pw = new PrintWriter("C:\\Users\\matte\\eclipse_workspace\\Oracle/src/output_rover_mon.txt");
+pw = new PrintWriter("C:\\Users\\matte\\eclipse_workspace\\Oracle _Copy/src/output_rover_mon.txt");
 
 root = new _cls_rover_mon0();
 _cls_rover_mon0_instances.put(root, root);
@@ -98,7 +98,7 @@ else if (no_automata < 0)
 }catch(Exception ex){ex.printStackTrace();}
 }
 
-int _state_id_Navigation = 41;
+int _state_id_Navigation = 5;
 
 public void _performLogic_Navigation(String _info, int... _event) {
 
@@ -106,45 +106,45 @@ _cls_rover_mon0.pw.println("[Navigation]AUTOMATON::> Navigation("+") STATE::>"+ 
 _cls_rover_mon0.pw.flush();
 
 if (0==1){}
-else if (_state_id_Navigation==37){
+else if (_state_id_Navigation==1){
 		if (1==0){}
-		else if ((_occurredEvent(_event,40/*send_command*/))){
+		else if ((_occurredEvent(_event,8/*send_command*/))){
 		_cls_rover_mon0.pw .println ("Sent navigation command to ROS");
 
-		_state_id_Navigation = 39;//moving to state sendExecToROS
+		_state_id_Navigation = 3;//moving to state sendExecToROS
 		_goto_Navigation(_info);
 		}
 }
-else if (_state_id_Navigation==41){
+else if (_state_id_Navigation==5){
 		if (1==0){}
-		else if ((_occurredEvent(_event,42/*check_nav*/))){
+		else if ((_occurredEvent(_event,10/*check_nav*/))){
 		_cls_rover_mon0.pw .println ("Checking the new navigation command data and obsticle distance!");
 
-		_state_id_Navigation = 36;//moving to state checkValid
+		_state_id_Navigation = 0;//moving to state checkValid
 		_goto_Navigation(_info);
 		}
 }
-else if (_state_id_Navigation==36){
+else if (_state_id_Navigation==0){
 		if (1==0){}
-		else if ((_occurredEvent(_event,34/*valid_command*/))){
+		else if ((_occurredEvent(_event,2/*valid_command*/))){
 		_cls_rover_mon0.pw .println ("Valid, send navigation command to the ROS System");
 
-		_state_id_Navigation = 37;//moving to state commandValid
+		_state_id_Navigation = 1;//moving to state commandValid
 		_goto_Navigation(_info);
 		}
-		else if ((_occurredEvent(_event,36/*invalid_command*/))){
+		else if ((_occurredEvent(_event,4/*invalid_command*/))){
 		_cls_rover_mon0.pw .println ("Invalid, send move on to the Control System");
 
-		_state_id_Navigation = 38;//moving to state commandInvalid
+		_state_id_Navigation = 2;//moving to state commandInvalid
 		_goto_Navigation(_info);
 		}
 }
-else if (_state_id_Navigation==39){
+else if (_state_id_Navigation==3){
 		if (1==0){}
-		else if ((_occurredEvent(_event,38/*send_move_on*/))){
+		else if ((_occurredEvent(_event,6/*send_move_on*/))){
 		_cls_rover_mon0.pw .println ("Sent send move on to the Control System");
 
-		_state_id_Navigation = 40;//moving to state sendMoveToNextCommand
+		_state_id_Navigation = 4;//moving to state sendMoveToNextCommand
 		_goto_Navigation(_info);
 		}
 }
@@ -157,16 +157,16 @@ _cls_rover_mon0.pw.flush();
 
 public String _string_Navigation(int _state_id, int _mode){
 switch(_state_id){
-case 40: if (_mode == 0) return "sendMoveToNextCommand"; else return "sendMoveToNextCommand";
-case 37: if (_mode == 0) return "commandValid"; else return "commandValid";
-case 36: if (_mode == 0) return "checkValid"; else return "checkValid";
-case 41: if (_mode == 0) return "getDistAndCommand"; else return "getDistAndCommand";
-case 39: if (_mode == 0) return "sendExecToROS"; else return "sendExecToROS";
-case 38: if (_mode == 0) return "commandInvalid"; else return "commandInvalid";
+case 4: if (_mode == 0) return "sendMoveToNextCommand"; else return "sendMoveToNextCommand";
+case 1: if (_mode == 0) return "commandValid"; else return "commandValid";
+case 0: if (_mode == 0) return "checkValid"; else return "checkValid";
+case 5: if (_mode == 0) return "getDistAndCommand"; else return "getDistAndCommand";
+case 3: if (_mode == 0) return "sendExecToROS"; else return "sendExecToROS";
+case 2: if (_mode == 0) return "commandInvalid"; else return "commandInvalid";
 default: return "!!!SYSTEM REACHED AN UNKNOWN STATE!!!";
 }
 }
-int _state_id_Speed = 47;
+int _state_id_Speed = 11;
 
 public void _performLogic_Speed(String _info, int... _event) {
 
@@ -174,45 +174,45 @@ _cls_rover_mon0.pw.println("[Speed]AUTOMATON::> Speed("+") STATE::>"+ _string_Sp
 _cls_rover_mon0.pw.flush();
 
 if (0==1){}
-else if (_state_id_Speed==43){
+else if (_state_id_Speed==7){
 		if (1==0){}
-		else if ((_occurredEvent(_event,40/*send_command*/))){
+		else if ((_occurredEvent(_event,8/*send_command*/))){
 		_cls_rover_mon0.pw .println ("Sent speed command to ROS");
 
-		_state_id_Speed = 45;//moving to state sendExecToROS
+		_state_id_Speed = 9;//moving to state sendExecToROS
 		_goto_Speed(_info);
 		}
 }
-else if (_state_id_Speed==47){
+else if (_state_id_Speed==11){
 		if (1==0){}
-		else if ((_occurredEvent(_event,44/*check_speed*/))){
+		else if ((_occurredEvent(_event,12/*check_speed*/))){
 		_cls_rover_mon0.pw .println ("Checking the new speed command data!");
 
-		_state_id_Speed = 42;//moving to state checkValid
+		_state_id_Speed = 6;//moving to state checkValid
 		_goto_Speed(_info);
 		}
 }
-else if (_state_id_Speed==42){
+else if (_state_id_Speed==6){
 		if (1==0){}
-		else if ((_occurredEvent(_event,34/*valid_command*/))){
+		else if ((_occurredEvent(_event,2/*valid_command*/))){
 		_cls_rover_mon0.pw .println ("Valid, send speed command to the ROS System");
 
-		_state_id_Speed = 43;//moving to state commandValid
+		_state_id_Speed = 7;//moving to state commandValid
 		_goto_Speed(_info);
 		}
-		else if ((_occurredEvent(_event,36/*invalid_command*/))){
+		else if ((_occurredEvent(_event,4/*invalid_command*/))){
 		_cls_rover_mon0.pw .println ("Invalid, send move on to the Control System");
 
-		_state_id_Speed = 44;//moving to state commandInvalid
+		_state_id_Speed = 8;//moving to state commandInvalid
 		_goto_Speed(_info);
 		}
 }
-else if (_state_id_Speed==45){
+else if (_state_id_Speed==9){
 		if (1==0){}
-		else if ((_occurredEvent(_event,38/*send_move_on*/))){
+		else if ((_occurredEvent(_event,6/*send_move_on*/))){
 		_cls_rover_mon0.pw .println ("Sent send move on to the Control System");
 
-		_state_id_Speed = 46;//moving to state sendMoveToNextCommand
+		_state_id_Speed = 10;//moving to state sendMoveToNextCommand
 		_goto_Speed(_info);
 		}
 }
@@ -225,16 +225,16 @@ _cls_rover_mon0.pw.flush();
 
 public String _string_Speed(int _state_id, int _mode){
 switch(_state_id){
-case 46: if (_mode == 0) return "sendMoveToNextCommand"; else return "sendMoveToNextCommand";
-case 43: if (_mode == 0) return "commandValid"; else return "commandValid";
-case 47: if (_mode == 0) return "getCommand"; else return "getCommand";
-case 42: if (_mode == 0) return "checkValid"; else return "checkValid";
-case 45: if (_mode == 0) return "sendExecToROS"; else return "sendExecToROS";
-case 44: if (_mode == 0) return "commandInvalid"; else return "commandInvalid";
+case 10: if (_mode == 0) return "sendMoveToNextCommand"; else return "sendMoveToNextCommand";
+case 7: if (_mode == 0) return "commandValid"; else return "commandValid";
+case 11: if (_mode == 0) return "getCommand"; else return "getCommand";
+case 6: if (_mode == 0) return "checkValid"; else return "checkValid";
+case 9: if (_mode == 0) return "sendExecToROS"; else return "sendExecToROS";
+case 8: if (_mode == 0) return "commandInvalid"; else return "commandInvalid";
 default: return "!!!SYSTEM REACHED AN UNKNOWN STATE!!!";
 }
 }
-int _state_id_Mast_Angle = 53;
+int _state_id_Mast_Angle = 17;
 
 public void _performLogic_Mast_Angle(String _info, int... _event) {
 
@@ -242,45 +242,45 @@ _cls_rover_mon0.pw.println("[Mast_Angle]AUTOMATON::> Mast_Angle("+") STATE::>"+ 
 _cls_rover_mon0.pw.flush();
 
 if (0==1){}
-else if (_state_id_Mast_Angle==49){
+else if (_state_id_Mast_Angle==13){
 		if (1==0){}
-		else if ((_occurredEvent(_event,40/*send_command*/))){
+		else if ((_occurredEvent(_event,8/*send_command*/))){
 		_cls_rover_mon0.pw .println ("Sent Mast command to ROS");
 
-		_state_id_Mast_Angle = 51;//moving to state sendExecToROS
+		_state_id_Mast_Angle = 15;//moving to state sendExecToROS
 		_goto_Mast_Angle(_info);
 		}
 }
-else if (_state_id_Mast_Angle==53){
+else if (_state_id_Mast_Angle==17){
 		if (1==0){}
-		else if ((_occurredEvent(_event,46/*check_vert_ang*/))){
+		else if ((_occurredEvent(_event,14/*check_vert_ang*/))){
 		_cls_rover_mon0.pw .println ("Checking the new Mast command data!");
 
-		_state_id_Mast_Angle = 48;//moving to state checkValid
+		_state_id_Mast_Angle = 12;//moving to state checkValid
 		_goto_Mast_Angle(_info);
 		}
 }
-else if (_state_id_Mast_Angle==48){
+else if (_state_id_Mast_Angle==12){
 		if (1==0){}
-		else if ((_occurredEvent(_event,34/*valid_command*/))){
+		else if ((_occurredEvent(_event,2/*valid_command*/))){
 		_cls_rover_mon0.pw .println ("Valid, send Mast command to the ROS System");
 
-		_state_id_Mast_Angle = 49;//moving to state commandValid
+		_state_id_Mast_Angle = 13;//moving to state commandValid
 		_goto_Mast_Angle(_info);
 		}
-		else if ((_occurredEvent(_event,36/*invalid_command*/))){
+		else if ((_occurredEvent(_event,4/*invalid_command*/))){
 		_cls_rover_mon0.pw .println ("Invalid, send move on to the Control System");
 
-		_state_id_Mast_Angle = 50;//moving to state commandInvalid
+		_state_id_Mast_Angle = 14;//moving to state commandInvalid
 		_goto_Mast_Angle(_info);
 		}
 }
-else if (_state_id_Mast_Angle==51){
+else if (_state_id_Mast_Angle==15){
 		if (1==0){}
-		else if ((_occurredEvent(_event,38/*send_move_on*/))){
+		else if ((_occurredEvent(_event,6/*send_move_on*/))){
 		_cls_rover_mon0.pw .println ("Sent send move on to the Control System");
 
-		_state_id_Mast_Angle = 52;//moving to state sendMoveToNextCommand
+		_state_id_Mast_Angle = 16;//moving to state sendMoveToNextCommand
 		_goto_Mast_Angle(_info);
 		}
 }
@@ -293,12 +293,12 @@ _cls_rover_mon0.pw.flush();
 
 public String _string_Mast_Angle(int _state_id, int _mode){
 switch(_state_id){
-case 52: if (_mode == 0) return "sendMoveToNextCommand"; else return "sendMoveToNextCommand";
-case 49: if (_mode == 0) return "commandValid"; else return "commandValid";
-case 53: if (_mode == 0) return "getCommand"; else return "getCommand";
-case 48: if (_mode == 0) return "checkValid"; else return "checkValid";
-case 51: if (_mode == 0) return "sendExecToROS"; else return "sendExecToROS";
-case 50: if (_mode == 0) return "commandInvalid"; else return "commandInvalid";
+case 16: if (_mode == 0) return "sendMoveToNextCommand"; else return "sendMoveToNextCommand";
+case 13: if (_mode == 0) return "commandValid"; else return "commandValid";
+case 17: if (_mode == 0) return "getCommand"; else return "getCommand";
+case 12: if (_mode == 0) return "checkValid"; else return "checkValid";
+case 15: if (_mode == 0) return "sendExecToROS"; else return "sendExecToROS";
+case 14: if (_mode == 0) return "commandInvalid"; else return "commandInvalid";
 default: return "!!!SYSTEM REACHED AN UNKNOWN STATE!!!";
 }
 }
